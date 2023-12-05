6a12d9c884d365186e14d19fa8449e521d5610c2
Sir-Cyborg <109237986+Sir-Cyborg@users.noreply.github.com>
2023-12-05 22:13:30 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -7,66 +7,69 @@ class ModelV0(nn.Module):
         super().__init__()
         self.block_1 = nn.Sequential(
             nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
                       stride=1),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=1,
                          stride=2)
         )
         self.block_2 = nn.Sequential(
-            nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+            nn.Conv1d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
                       stride=1),
             nn.ReLU(),
-            nn.LocalResponseNorm(),
+            nn.LocalResponseNorm(size=5, alpha=0.0002, beta=0.75, k=1.0),
             nn.MaxPool1d(kernel_size=1,
                          stride=2)
         )
         self.block_3 = nn.Sequential(
-            nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+            nn.Conv1d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
                       stride=1),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=1,
                          stride=2)
         )
         self.block_4 = nn.Sequential(
-            nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+            nn.Conv1d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3, 
                       stride=1),
             nn.ReLU(),
-            nn.LocalResponseNorm(),
+            nn.LocalResponseNorm(size=5, alpha=0.0002, beta=0.75, k=1.0),
         )
         self.block_5 = nn.Sequential(
-            nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+            nn.Conv1d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
                       stride=1),
             nn.ReLU()
         )
         self.block_6 = nn.Sequential(
-            nn.Conv1d(in_channels=input_shape, 
-                      out_channels=hidden_units, 
+            nn.Conv1d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
                       stride=1),
             nn.ReLU(),
-            nn.LocalResponseNorm(),
+            nn.LocalResponseNorm(size=5, alpha=0.0002, beta=0.75, k=1.0),
             nn.Dropout(0.5)
         )
 
         self.classifier = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(in_features=hidden_units*7*7, 
-                      out_features=output_shape)
+            nn.Linear(in_features=hidden_units, 
+                      out_features=output_shape),
+            nn.Softmax()
         )
     
     def forward(self, x: torch.Tensor):
         x = self.block_1(x)
-        # print(x.shape)
         x = self.block_2(x)
-        # print(x.shape)
         x = self.block_3(x)
         x = self.block_4(x)
         x = self.block_5(x)
         x = self.block_6(x)
         x = self.classifier(x)
-        # print(x.shape)
         return x
