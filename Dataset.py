fb9a8a7c0b59e32e37c8c73360bcd3733b68d611
Sir-Cyborg <109237986+Sir-Cyborg@users.noreply.github.com>
2023-12-11 19:25:27 +0100

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -4,30 +4,44 @@ import wfdb
 import torch
 from torch.utils.data import Dataset
 from scipy.signal import butter, filtfilt, iirnotch
-
-import plot
+import os
 
 class ECGDataset(Dataset):
-    def __init__(self, csv_path, ecg_data_path, split) -> None:
-        self.patients_data = pd.read_csv(csv_path, index_col='ecg_id')  #il dataframe
+    def __init__(self, csv_path, ecg_data_path, split):
+        self.patients_data = pd.read_csv(csv_path, index_col='ecg_id')  # dataframe of metadata
         self.ecg_data_path = ecg_data_path
-        self.ecg_signals, self.ids = self.load_ecg_signals() #super lista #numpy.ndarray
+        self.ecg_signals, self.ids = self.load_ecg_signals() # numpy.ndarray of ecg records, and ids
         self.split = split
 
     def load_ecg_signals(self):
-        ecg_signals = []
-        ids = []
-        for index, patient_info in self.patients_data.iterrows():
-            filename_lr = patient_info['filename_lr']
-            record = wfdb.rdrecord(f"{self.ecg_data_path}/{filename_lr}")
-            ecg_signal = record.p_signal[:, [0]] 
-            ecg_signal = self.filter_signal(ecg_signal)
-            ecg_signals.append(ecg_signal)
-            ids.append(patient_info['patient_id'])
+        cache_path = 'cache' 
+        # make dir
+        if not os.path.exists(cache_path):
+            os.makedirs(cache_path)
+        # check if already exist
+        signals_file = os.path.join(cache_path, 'ecg_signals.npy')
+        ids_file = os.path.join(cache_path, 'ids.npy')
+        if os.path.exists(signals_file) and os.path.exists(ids_file):
+            ecg_signals = np.load(signals_file)
+            ids = np.load(ids_file)
+        else:
+            ecg_signals = []
+            ids = []
+            for index, patient_info in self.patients_data.iterrows():
+                filename_lr = patient_info['filename_lr']
+                record = wfdb.rdrecord(f"{self.ecg_data_path}/{filename_lr}")
+                ecg_signal = record.p_signal[:, [0]] # for now we select only one lead
+                ecg_signal = self.filter_signal(ecg_signal)
+                ecg_signals.append(ecg_signal)
+                ids.append(patient_info['patient_id'])
 
-        ecg_signals = np.array(ecg_signals)
-        ecg_signals = np.swapaxes(ecg_signals, 1, 2)
-        ids = np.array(ids)
+            ecg_signals = np.array(ecg_signals)
+            ecg_signals = np.swapaxes(ecg_signals, 1, 2)
+            ids = np.array(ids)
+
+            # save on disk
+            np.save(signals_file, ecg_signals)
+            np.save(ids_file, ids)
         return ecg_signals, ids
 
     def cut_signal(self, signal):
@@ -48,11 +62,11 @@ class ECGDataset(Dataset):
 
         # Restituisci parti del segnale basate sulla modalità di split
         if self.split == 'first_half':
-            return torch.tensor(part1).type(torch.float), patient_id #dava problemi
+            return torch.tensor(part1).type(torch.float), torch.tensor(patient_id) #dava problemi
             #return torch.from_numpy(part1.copy()).type(torch.float32), patient_id
         elif self.split == 'second_half':
             #return torch.from_numpy(part2.copy()).type(torch.float32), patient_id
-            return torch.tensor(part2).type(torch.float), patient_id
+            return torch.tensor(part2).type(torch.float), torch.tensor(patient_id)
 
     def filter_signal(self, signal, fs=100):
         [b, a] = butter(3, (0.5, 40), btype='bandpass', fs=fs)
@@ -60,3 +74,5 @@ class ECGDataset(Dataset):
         [bn, an] = iirnotch(50, 3, fs=fs)
         signal = filtfilt(bn, an, signal, axis=0)
         return signal
+    
+#train_dataset = ECGDataset("primoecg.csv", "archive/", split='first_half')
